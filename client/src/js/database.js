1477be38df8b37d5975ec3dac8691c5ebd082099
Rachel Harris <rachel.harris@msn.com>
2022-04-23 15:05:25 +1000

    Update comments in database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Accept some content and add it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
 
